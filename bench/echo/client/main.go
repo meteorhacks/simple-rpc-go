4adf5ab294fae3a554c0dcc8fae24a3309efdbba
Muhammed Thanish <mnmtanish@gmail.com>
2015-07-21 10:36:27 +0530

    Refactor benchmark code
     - add pprof
     - optimize benchmark code

---
+++ bench/echo/client/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	_ "net/http/pprof"
+
+	"github.com/meteorhacks/simple-rpc-go"
+)
+
+const (
+	concurrency = 8
+)
+
+func main() {
+	for i := 0; i < concurrency; i++ {
+		go startClient()
+	}
+
+	startPPROF()
+}
+
+func startClient() {
+	c := srpc.NewClient("localhost:12345")
+	p := make([]byte, 1024, 1024)
+
+	if err := c.Connect(); err != nil {
+		panic(err)
+	}
+
+	for {
+		if _, err := c.Call("echo", p); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func startPPROF() {
+	log.Println("PPROF:  listening on :6061")
+	log.Println(http.ListenAndServe(":6061", nil))
+}
---
+++ bench/echo/server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"sync/atomic"
+	"time"
+
+	"net/http"
+	_ "net/http/pprof"
+
+	"github.com/meteorhacks/simple-rpc-go"
+)
+
+var (
+	counter uint32
+)
+
+func main() {
+	go startPPROF()
+	go logRate()
+
+	s := srpc.NewServer(":12345")
+	s.SetHandler("echo", handler)
+	log.Println(s.Listen())
+}
+
+func handler(req []byte) (res []byte, err error) {
+	atomic.AddUint32(&counter, 1)
+	return req, nil
+}
+
+func logRate() {
+	for {
+		time.Sleep(time.Second * 10)
+		log.Println(counter/10, "req/s")
+		atomic.StoreUint32(&counter, 0)
+	}
+}
+
+func startPPROF() {
+	log.Println("PPROF:  listening on :6060")
+	log.Println(http.ListenAndServe(":6060", nil))
+}
